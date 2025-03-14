693ea8e3570fcfe1b1643964d0940544628616d1
Dima Zangana <zanganadima@gmail.com>
2025-03-14 12:28:38 +0000

    [DEV] Add object scripts

---
+++ Assets/_Game/Core/Scripts/Objects/InputConfig.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace _Game.Core.Scripts.Objects
+{
+    [CreateAssetMenu]
+    public class InputConfig : ScriptableObject
+    {
+        public int MouseShootButton;
+        public int MouseAimButton;
+        
+        public KeyCode JumpKey = KeyCode.W;
+        public KeyCode ReloadKey = KeyCode.R;
+    }
+}
---
+++ Assets/_Game/Core/Scripts/Objects/WeaponSO.cs
@@ -0,0 +1,16 @@
+﻿using _Game.Core.Scripts.Enums;
+using UnityEngine;
+
+namespace _Game.Core.Scripts.Objects
+{
+    [CreateAssetMenu(fileName = "New Weapon", menuName = "New Weapon", order = 0)]
+    public class WeaponSO : ScriptableObject
+    {
+        public Weapons WeaponType;
+        public GameObject Prefab;
+        public float Cooldown;
+        public int MaxAmmo;
+        public int WeaponId;
+        public float XFiringOffset;
+    }
+}
