f3d520ab9e8146c07ee323efe8ead7f3f9b1eb00
Dima Zangana <zanganadima@gmail.com>
2025-03-14 12:28:16 +0000

    [DEV] Add gameplay scripts

---
+++ Assets/_Game/Core/Scripts/Gameplay/InteractableBox.cs
@@ -0,0 +1,30 @@
+﻿using _Game.Core.Scripts.Interfaces;
+using UnityEngine;
+
+namespace _Game.Core.Scripts.Gameplay
+{
+    public class InteractableBox : MonoBehaviour, IInvertable
+    {
+        private Rigidbody2D _rigidbody;
+        public float DefaultGravity { get; set; } = 5;
+        public float InvertedGravity { get; set; } = -5;
+        public bool IsInverted { get; private set; }
+
+        private void Awake()
+        {
+            Initialise();
+        }
+
+        private void Initialise()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        public void Invert()
+        {
+            IsInverted = !IsInverted;
+
+           _rigidbody.gravityScale = IsInverted ? InvertedGravity : DefaultGravity;
+        }
+    }
+}
---
+++ Assets/_Game/Core/Scripts/Gameplay/InversionTrigger.cs
@@ -0,0 +1,31 @@
+﻿using _Game.Core.Scripts.Interfaces;
+using UnityEngine;
+
+namespace _Game.Core.Scripts.Gameplay
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class InversionTrigger : MonoBehaviour
+    {
+        [Header("VFX")]
+        [SerializeField] private ParticleSystem _activationEffect; //TODO: I need to add a VFX and maye some SFX too
+        
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.TryGetComponent<IInvertable>(out var invertable))
+            {
+                float test = transform.position.y - other.transform.position.y;
+
+                switch (test)
+                {
+                    case > 0.1f when !invertable.IsInverted:
+                    case < -0.1f when invertable.IsInverted:
+                        invertable.Invert();
+                        break;
+                }
+                
+                if (_activationEffect != null)
+                    Instantiate(_activationEffect, transform.position, transform.rotation);
+            }
+        }
+    }
+}
