d877a67e691bc63f3a701fe6d26c8814fc5d0542
Dima Zangana <zanganadima@gmail.com>
2025-03-14 12:27:49 +0000

    [DEV] Add combat scripts

---
+++ Assets/_Game/Core/Scripts/Combat/Bullet.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using _Game.Core.Scripts.Interfaces;
+using UnityEngine;
+
+namespace _Game.Core.Scripts.Combat
+{
+    public class Bullet : MonoBehaviour, IPoolable, IInvertable
+    {
+        private Rigidbody2D _rigidbody;
+        
+        [SerializeField] private float _speed = 10f;
+        [SerializeField] private float _lifeTime = 2f;
+        
+        [SerializeField] private LayerMask _layerMask;
+        
+        public float DefaultGravity { get; set; } = 5;
+        public float InvertedGravity { get; set; } = -5;
+        public bool IsInverted { get; private set; }
+
+        public Action<IPoolable> OnReturnToPool { get; set; }
+        
+        private float _timer;
+        
+        private void Awake() => _rigidbody = GetComponent<Rigidbody2D>();
+
+        public void Initialise()
+        {
+            gameObject.SetActive(true);
+            _timer = _lifeTime;
+        }
+        
+        private void Update()
+        {
+            CheckTimerAndReturnToPool();
+        }
+        
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.layer != _layerMask)
+                return;
+
+            OnReturnToPool?.Invoke(this);
+        }
+        
+        public void Invert()
+        {
+            IsInverted = !IsInverted;
+
+            _rigidbody.gravityScale = IsInverted ? InvertedGravity : DefaultGravity;
+        }
+
+        public void ResetObject()
+        {
+            if (IsInverted)
+                Invert();
+            
+            _rigidbody.linearVelocity = Vector2.zero;
+            gameObject.SetActive(false);
+
+        }
+
+        public void SetDirection(Vector2 direction)
+        {
+            _rigidbody.linearVelocity = direction * _speed;
+        }
+
+        private void CheckTimerAndReturnToPool()
+        {
+            _timer -= Time.deltaTime;
+            
+            if (_timer <= 0) 
+                OnReturnToPool?.Invoke(this);
+        }
+    }
+}
---
+++ Assets/_Game/Core/Scripts/Combat/RangedWeaponBase.cs
@@ -0,0 +1,49 @@
+﻿using _Game.Core.Scripts.Interfaces;
+using _Game.Core.Scripts.Objects;
+using _Game.Core.Scripts.Pooling;
+using UnityEngine;
+
+namespace _Game.Core.Scripts.Combat
+{
+    public abstract class RangedWeaponBase : MonoBehaviour, IRangedWeapon
+    {
+        [SerializeField] protected WeaponSO _config;
+        [SerializeField] protected Transform _firePoint;
+    
+        protected ObjectPool<Bullet> _projectilePool;
+        protected int _currentAmmo;
+        protected float _lastFireTime;
+
+        public int CurrentAmmo => _currentAmmo;
+        public int MaxAmmo => _config.MaxAmmo;
+        public float AttackCooldown => _config.Cooldown;
+        public bool CanAttack => Time.time > _lastFireTime + AttackCooldown && _currentAmmo > 0;
+        public int WeaponID => _config.WeaponId;
+
+        protected virtual void Awake()
+        {
+            _currentAmmo = MaxAmmo;
+            InitializePool();
+        }
+        
+        protected virtual void InitializePool()
+        {
+            Bullet prefab = Resources.Load<Bullet>(nameof(Bullet));
+            _projectilePool = new ObjectPool<Bullet>(prefab, 10, transform);
+        }
+
+        public virtual void Attack()
+        {
+            if (!CanAttack) return;
+        
+            Bullet bullet = _projectilePool.Get();
+            ConfigureBullet(bullet);
+            _currentAmmo--;
+            _lastFireTime = Time.time;
+            bullet.transform.parent = null;
+        }
+
+        protected abstract void ConfigureBullet(Bullet bullet);
+        public virtual void Reload() => _currentAmmo = MaxAmmo;
+    }
+}
